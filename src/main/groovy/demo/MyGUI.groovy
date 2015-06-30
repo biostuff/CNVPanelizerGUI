6a161ad9a08b90f11205f7bb8aac73564771ae46
tanovsky <cristiano.oliveira@med.uni-heidelberg.de>
2015-06-30 13:11:36 +0200

    Fix bug. crashing when save configuration from scratch

--- src/main/groovy/demo/MyGUI.groovy
+++ src/main/groovy/demo/MyGUI.groovy
@@ -65,19 +65,20 @@ import groovy.io.FileType
 import groovy.json.JsonOutput
 import groovy.json.JsonSlurper
 
+
 class MainWindow extends JFrame {
 
     MainWindow window
-    JTextField sampleDirectoryTextField = new JTextField("")
+    JTextField sampleDirectoryTextField = new JTextField()
     JButton sampleDirectoryButton = new JButton("Sample")
     JButton referenceDirectoryButton = new JButton("Reference")
     JTextField referenceDirectoryTextField = new JTextField("")
     JList referenceList
     JList sampleList
     JButton bedFilepathButton = new JButton("...")
-    JTextField bedFilepathTextField = new JTextField("")
+    JTextField bedFilepathTextField = new JTextField()
     //    JTextField bedGeneNameColumnNumber = new JTextField("6")
-    JTextField outputDirTextField = new JTextField("")
+    JTextField outputDirTextField = new JTextField()
     JButton okButton = new JButton("Run")
     JButton cancelButton = new JButton("Cancel")
     JButton outputDirectoryButton = new JButton("...")
@@ -123,6 +124,10 @@ class MainWindow extends JFrame {
         menuItemExport.addActionListener(new java.awt.event.ActionListener() {
                 public void actionPerformed(java.awt.event.ActionEvent evt) {
 
+                    println sampleList
+                    println sampleList.getModel().class
+//                    println a
+
                     DefaultListModel sListModel = (DefaultListModel)sampleList.getModel()
                     String[]  sListModelArray = sListModel.toArray()
                     List sList = Arrays.asList(sListModelArray)
@@ -141,21 +146,24 @@ class MainWindow extends JFrame {
                     println "referenceSet " + rList
                     println "--------------------"
 
+                    String bedFilepath = bedFilepathTextField.getText()
+                    String outputDirectoryPath = outputDirTextField.getText()
+
                     def json = JsonOutput.toJson(
                             "params": [
-                                "bed": bedFilepathTextField.getText(),
+                                "bed": bedFilepath,
                                 "ampliconNameColumnNumber": ampliconNameColumnNumber,
                                 "removePcrDuplicates": removePcrDuplicates,
                                 "specificityLevel": specificityLevel,
                                 "numberOfBootstrapReplicates": numberOfBootstrapReplicates,
-                                "outputDir": outputDirTextField.getText(),
+                                "outputDir": outputDirectoryPath,
                             //                                "overwriteResults": overwriteResults,
                                 "sampleSet": sList,
                                 "referenceSet": rList
                         ])
                     String prettyJson = JsonOutput.prettyPrint(json)
-                    println  prettyJson
-                    
+                    println prettyJson
+
                     JFileChooser fileChooser = new JFileChooser()
                     fileChooser.setCurrentDirectory(new java.io.File("."));
                     fileChooser.setDialogTitle("Specify a file to save the parameters")  
@@ -216,6 +224,7 @@ class MainWindow extends JFrame {
         panelReference.add(panelReferenceSet, BorderLayout.NORTH)
         
         referenceList = new JList()
+        referenceList.setModel(new DefaultListModel())
         referenceList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
         //        referenceList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
         //list.setLayoutOrientation(JList.HORIZONTAL_WRAP);
@@ -280,6 +289,7 @@ class MainWindow extends JFrame {
         painelSample.add(painelSampleSet, BorderLayout.NORTH)
 
         sampleList = new JList()
+        sampleList.setModel(new DefaultListModel())
         sampleList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
         //        sampleList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
         //list.setLayoutOrientation(JList.HORIZONTAL_WRAP);
@@ -650,6 +660,93 @@ class MainWindow extends JFrame {
         worker.execute()
     }
 
+/**
+ * Unescapes a string that contains standard Java escape sequences.
+ * <ul>
+ * <li><strong>\b \f \n \r \t \" \'</strong> :
+ * BS, FF, NL, CR, TAB, double and single quote.</li>
+ * <li><strong>\X \XX \XXX</strong> : Octal character
+ * specification (0 - 377, 0x00 - 0xFF).</li>
+ * <li><strong> </strong> : Hexadecimal based Unicode character.</li>
+ * </ul>
+ * 
+ * @param st
+ *            A string optionally containing standard java escape sequences.
+ * @return The translated string.
+ */
+//public String unescapeJavaString(String st) {
+// 
+//    StringBuilder sb = new StringBuilder(st.length());
+// 
+//    for (int i = 0; i < st.length(); i++) {
+//        char ch = st.charAt(i);
+//        if (ch == '\\') {
+//            char nextChar = (i == st.length() - 1) ? '\\' : st
+//                    .charAt(i + 1);
+//            // Octal escape?
+//            if (nextChar >= '0' && nextChar <= '7') {
+//                String code = "" + nextChar;
+//                i++;
+//                if ((i < st.length() - 1) && st.charAt(i + 1) >= '0'
+//                        && st.charAt(i + 1) <= '7') {
+//                    code += st.charAt(i + 1);
+//                    i++;
+//                    if ((i < st.length() - 1) && st.charAt(i + 1) >= '0'
+//                            && st.charAt(i + 1) <= '7') {
+//                        code += st.charAt(i + 1);
+//                        i++;
+//                    }
+//                }
+//                sb.append((char) Integer.parseInt(code, 8));
+//                continue;
+//            }
+//            switch (nextChar) {
+//            case '\\':
+//                ch = '\\';
+//                break;
+//            case 'b':
+//                ch = '\b';
+//                break;
+//            case 'f':
+//                ch = '\f';
+//                break;
+//            case 'n':
+//                ch = '\n';
+//                break;
+//            case 'r':
+//                ch = '\r';
+//                break;
+//            case 't':
+//                ch = '\t';
+//                break;
+//            case '\"':
+//                ch = '\"';
+//                break;
+//            case '\'':
+//                ch = '\'';
+//                break;
+//            // Hex Unicode: u????
+//            case 'u':
+//                if (i >= st.length() - 5) {
+//                    ch = 'u';
+//                    break;
+//                }
+//                int code = Integer.parseInt(
+//                        "" + st.charAt(i + 2) + st.charAt(i + 3)
+//                                + st.charAt(i + 4) + st.charAt(i + 5), 16);
+//                sb.append(Character.toChars(code));
+//                i += 5;
+//                continue;
+//            }
+//            i++;
+//        }
+//        sb.append(ch);
+//    }
+//    return sb.toString();
+//}
+    
+    
+    
     class MyMouseListener extends MouseAdapter {
         
         JList list
